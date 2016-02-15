16d25d8227b176b7af0da75d23860dd29c41ea28
Joshua Book <joshua.book@gmail.com>
2016-02-15 18:43:56 -0500

    autoload lib

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ZypeApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # autoload lib
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
