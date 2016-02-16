9faaed81e2f197228f724e4717eff24fd86e2241
Joshua Book <joshua.book@gmail.com>
2016-02-16 09:54:16 -0500

    add video model

---
+++ app/models/video.rb
@@ -0,0 +1,10 @@
+class Video
+  def initialize(zype_id, title, description, sub_req, published_at, thumbnails)
+    @zype_id = zype_id
+    @title = title
+    @description = description
+    @sub_req = sub_req
+    @published_at = published_at
+    @thumbnails = thumbnails
+  end
+end
