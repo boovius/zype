c89c9be44a0febefed9c38e27f10d06d608771c2
Joshua Book <joshua.book@gmail.com>
2016-02-17 12:44:03 -0500

    add helper method for throwing toasts into html when flash exists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def toastr_flash
+    flash_messages = []
+    flash.keys.each do |key|
+      message = flash[key]
+      type = 'success' if key == 'success'
+      type = 'info'    if key == 'notice'
+      type = 'warning' if key == 'alert'
+      type = 'error'   if key == 'error'
+      text = "<script>toastr.#{type}('#{message}');</script>"
+      flash_messages << text.html_safe if message
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
