52ab66ba9d1897e0dacbc821fd5ea1ef41a09bee
Joshua Book <joshua.book@gmail.com>
2016-02-16 21:49:19 -0500

    build out create session method

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,5 +3,30 @@ class SessionController < ApplicationController
   end
 
   def create
+    response = AuthService.get_access_token (token_parameters)
+    if response.success?
+      session['access_token'] = response.data[:access_token]
+      session['expires_in'] = response.data[:expires_in]
+
+      redirect_to "/videos/#{session['zype_id']}/true"
+    else
+      redirect_to '/login'
+    end
+    # take params
+    # send to zype api
+      #if valid
+        # get access token
+        # store access_token in session
+    # else
+        # display login error message to user
+  end
+
+  private
+
+  def token_parameters
+    {
+      username: params['username'],
+      password: params['password'],
+    }
   end
 end
