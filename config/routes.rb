d82b45e873835001ac4b3105e9442ff12494fb34
Joshua Book <joshua.book@gmail.com>
2016-02-17 11:45:23 -0500

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'videos#index'
 
   get 'videos/:zype_id/:sub_req' => 'videos#show'
-  get  'login' => 'session#new'
-  post 'login' => 'session#create'
+  get  'login'  => 'session#new'
+  post 'login'  => 'session#create'
+  get  'logout' => 'session#destroy'
 
   get '*path' => redirect('/')
 end
